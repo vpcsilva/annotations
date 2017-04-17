526aff506a2b5e43468db985cb28ee153d5136b6
vpcsilva <vpcx5@yahoo.com.br>
2017-04-17 10:43:21 -0300

    Load css files from libraris in the javascript

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import VueResource from 'vue-resource';
 
 import routes from './routes';
 
+import leafletCss from 'leaflet/dist/leaflet.css';
+import leafletDrawCss from 'leaflet-draw/dist/leaflet.draw.css';
+import contextmenuCss from 'leaflet-contextmenu/dist/leaflet.contextmenu.min.css';
+
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
