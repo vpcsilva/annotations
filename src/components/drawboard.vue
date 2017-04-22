bb3c82419ea796d9f8e3dccb3d758b490d56da03
vpcsilva <vpcx5@yahoo.com.br>
2017-04-22 11:51:50 -0300

    Changing from leaflet-draw to leaflet-editable

--- src/components/drawboard.vue
+++ src/components/drawboard.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
     <div :id="id" class="map"></div>
-    <Modal :title="modalTitle" :show="showModal" @ok="modalOk" @cancel="modalCancel">
-      <textarea name="" id="" cols="30" rows="3" class="form-control" v-model="markerMessage"></textarea>
+    <Modal :title="modalTitle" :show="modalVisible" @ok="modalOk" @cancel="modalCancel">
+      <textarea name="" id="markerMessageInput" cols="30" rows="3" class="form-control" v-model="markerMessage" autofocus></textarea>
     </Modal>
   </div>
 </template>
 
 <script>
   import EventBus from '../eventBus.js';
-  import L from 'leaflet';
-  import LD from 'leaflet-draw';
   import Modal from './modal.vue';
-  import CM from 'leaflet-contextmenu';
 
-  // import GU from 'leaflet-geometryutil';
-  // import Snap from 'leaflet-snap';
+  import L from 'leaflet';
+  import {} from 'leaflet-contextmenu';
+  import {} from 'leaflet.path.drag';
+  import {} from 'leaflet-editable';
 
   export default {
     props: ['image', 'index'],
@@ -28,19 +27,26 @@
         id: 'map'+this._uid,
         itensOnMap: false,
         drawItems: null,
-        showModal: false,
-        images: [],
-        componentIndex: this.index,
+        modalVisible: false,
+        annotations: [],
+        drawboardIndex: this.index,
         imageOverlay: false,
         markerMessage: '',
-        modalTitle: 'Comment'
+        modalTitle: 'Comment',
+        layer: true,
       }
     },
     watch: {
-      image: function(newImg, oldImg) {
+      image: function(newImg, oldImg) { // Changes before event changeDrawboard
         this.imgWidth = newImg.width;
         this.imgHeight = newImg.height;
         this.setImage();
+      },
+      drawItems: function(newVal, oldVal) {
+        if (oldVal) {
+         this.map.removeLayer(oldVal); 
+        }
+        newVal.addTo(this.map);
       }
     },
     created () {
@@ -48,6 +54,9 @@
       EventBus.$on('changeDrawboard', this.changeDrawboard);
       EventBus.$on('save', this.export);
       EventBus.$on('import', this.retrieve);
+      EventBus.$on('addRectangle', this.addRectangle);
+      EventBus.$on('addMarker', this.addMarker);
+      EventBus.$on('toggleLayer', this.toggleLayer);
     },
     mounted () {
       this.setupMap();
@@ -58,63 +67,14 @@
           crs: L.CRS.Simple,
           maxZoom: 5,
           minZoom: -5,
+          editable: true,
         };
 
         this.map = L.map(this.id, options);
 
-        let drawItems = L.featureGroup().addTo(this.map);
+        let drawItems = L.featureGroup();
         this.bounds = [[0,0],[this.imgHeight, this.imgWidth]];
         this.imageOverlay = L.imageOverlay(this.image.src, this.bounds).addTo(this.map);
-
-        let control = new L.Control.Draw({
-          edit: {
-            featureGroup: drawItems,
-            poly: {
-              allowIntersection: false,
-            },
-          },
-          draw: {
-            polyline: false,
-            polygon: false,
-            circle: false,
-          },
-        });
-
-        window.drawItems = drawItems;
-        this.map.addControl(control);
-
-        this.map.on(L.Draw.Event.CREATED, (e) => {
-          let layer = e.layer;
-          let type = e.layerType;
-
-          this.showModal = true;
-          this.afterOk = () => {
-            layer.bindPopup(this.markerMessage);
-            drawItems.addLayer(layer);
-          }
-          this.afterCancel = () => {
-            console.log('Item canceled');
-          }
-          layer.bindContextMenu({
-            contextmenu: true,
-            contextmenuItems: [
-              {
-                text: 'Edit Comment',
-                callback: () => {
-                  this.markerMessage = layer.getPopup().getContent();
-                  this.showModal = true;
-                  this.afterOk = () => {
-                    layer.setPopupContent(this.markerMessage);
-                  }
-                }
-              },
-              '-',
-              {
-                text: 'Close'
-              }
-            ]
-          })
-        });
 
         this.drawItems = drawItems;
         this.map.fitBounds(this.bounds);
@@ -130,22 +90,20 @@
         this.imageOverlay.setBounds(this.createLatLngBounds(this.bounds));
       },
       changeDrawboard (index) {
-        if (index == this.index) {
-          this.isActive = true;
-        } else {
-          this.isActive = false;
+        if (this.annotations[index]) {
+          
         }
-        this.componentIndex = index;
+        this.drawboardIndex = index;
       },
       export () {
-        this.showModal = true;
+        this.showModal();
         this.modalTitle = 'Map Data';
         this.markerMessage = JSON.stringify(this.drawItems.toGeoJSON());
         window.geoJson = this.drawItems.toGeoJSON();
         console.log(this.drawItems.toGeoJSON());
       },
       retrieve () {
-        this.showModal = true;
+        this.showModal();
         this.modalTitle = 'Import Data';
         this.afterOk = () => {
           let geoJson = L.geoJSON(JSON.parse(this.markerMessage));
@@ -153,7 +111,7 @@
         }
       },
       modalOk () {
-        this.showModal = false;
+        this.hideModal();
         if (this.afterOk) {
           this.afterOk();
         }
@@ -161,12 +119,91 @@
         this.afterOk = null;
       },
       modalCancel () {
-        this.showModal = false;
+        this.hideModal();
         if (this.afterOk) {
           this.afterCancel();
         }
         this.markerMessage = '';
         this.afterCancel = null;
+      },
+      showModal () {
+        this.modalVisible = true;
+        document.getElementById('markerMessageInput').focus();
+      },
+      hideModal () {
+        this.modalVisible = false;
+      },
+      addRectangle () {
+        let rect = this.map.editTools.startRectangle();
+    
+        rect.on('editable:drawing:commit', (e) => {
+          this.showModal();
+          this.afterOk = () => {
+            rect.bindPopup(this.markerMessage);
+            this.drawItems.addLayer(rect);
+          };
+          this.afterCancel = () => {
+            console.log('Item canceled');
+            this.map.removeLayer(rect);
+          };
+          rect.bindContextMenu(this.createContextMenu(rect));
+        });
+        // rect.disableEdit();
+      },
+      addMarker () {
+        let marker = this.map.editTools.startMarker();
+        marker.on('editable:drawing:commit', (e) => {
+          this.showModal();
+          this.afterOk = () => {
+            marker.bindPopup(this.markerMessage);
+            this.drawItems.addLayer(marker);
+          };
+          this.afterCancel = () => {
+            console.log('Item canceled');
+            this.map.removeLayer(marker);
+          };
+          marker.bindContextMenu(this.createContextMenu(marker));
+        });
+      },
+      createContextMenu (layer) {
+        let options = {
+          contextmenu: true,
+          contextmenuItems: [
+            {
+              text: 'Edit Comment',
+              callback: () => {
+                this.markerMessage = layer.getPopup().getContent();
+                this.showModal();
+                this.afterOk = () => {
+                  layer.setPopupContent(this.markerMessage);
+                }
+              }
+            },
+            {
+              text: 'Delete',
+              callback: () => {
+                this.drawItems.removeLayer(layer);
+              }
+            },
+            '-',
+            {
+              text: 'Close'
+            }
+          ]
+        };
+        return options;
+      },
+      toggleLayer () {
+        if(this.layer) {
+          console.log('REMOVE LAYER', this.drawItems);
+          this.map.removeLayer(this.drawItems);
+          this.layer = !this.layer;
+        }
+        else {
+          console.log('ADD LAYER', this.drawItems);
+          this.drawItems.addTo(this.map);
+          this.layer = !this.layer;
+        }
       }
     },
     components: {
